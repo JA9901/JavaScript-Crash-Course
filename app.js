833c4a16c82bc0bd1c6381be03d7f3ba7c382049
JA9901 <jareyes999@gmail.com>
2022-08-01 23:48:00 -0400

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let dollars = [1, 5, 10, 3]
 let cents = []
 
 for (let i = 0; i < dollars.length; i++){
-    cents.push(dollars[i] * 100)
+    cents.push(dollars[i] * 100000)
 }
 
 console.log(cents)
